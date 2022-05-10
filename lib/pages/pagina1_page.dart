beaef8c16d4d66ab0fc7213bf27eaa78ce58558a
Jose Estrada <jose.estrada@foodcoin.global>
2022-05-10 18:08:55 +0200

    getx acabado

--- lib/pages/pagina1_page.dart
+++ lib/pages/pagina1_page.dart
@@ -1,24 +1,57 @@
+import 'package:estados/controllers/usuarios_controller.dart';
+import 'package:estados/models/usuario.dart';
+import 'package:estados/pages/pagina2_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
 
 class Pagina1Page extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+
+  final usuarioCtrl = Get.put(UsuarioController());
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pagina1'),
+        title: Text('Pagina 1'),
       ),
-      body: InformacionUsuarios(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.accessibility_new),
-          onPressed: () => Navigator.pushNamed(context, 'pagina2')
-          ),
+      body: Obx(
+        () => usuarioCtrl.existeUsuario.value
+        ? InformacionUsuario(usuario: usuarioCtrl.usuario.value)
+        : NoInfo()
+        ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.accessibility_new),
+        //onPressed: () => Navigator.pushNamed(context, 'pagina2')
+        //onPressed:  () => Get.to(Pagina2Page()),
+        onPressed: () =>
+            Get.toNamed('pagina2', arguments: {
+              'nombre': 'Jose', 
+              'edad': 32
+              }),
+      ),
     );
   }
 }
 
-class InformacionUsuarios extends StatelessWidget {
+class NoInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('No hay usuario seleccionado'),
+      ),
+    );
+  }
+}
+
+class InformacionUsuario extends StatelessWidget {
+
+  final Usuario usuario;
+
+  const InformacionUsuario({
+    Key? key, 
+    required this.usuario
+    }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -29,21 +62,20 @@ class InformacionUsuarios extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-
-          Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          Text('General',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           Divider(),
-          ListTile(title: Text('Nombre: ')),
-          ListTile(title: Text('Edad: ')),
-
-          Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+          ListTile(title: Text('Nombre: ${this.usuario.nombre}')),
+          ListTile(title: Text('Edad: ${this.usuario.edad}')),
+          Text('Profesiones',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           Divider(),
-          ListTile(title: Text('Profesion 1')),
-          ListTile(title: Text('Profesion 1')),
-          ListTile(title: Text('Profesion 1')),
-
-
-
-
+          ...usuario.profesiones.map((profesion)=> ListTile(
+            title: Text(profesion,)
+          ))
+          //ListTile(title: Text('Profesion 1')),
+          //ListTile(title: Text('Profesion 1')),
+          //ListTile(title: Text('Profesion 1')),
         ],
       ),
     );
